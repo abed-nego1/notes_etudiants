25e7798e5f5f3254edbae35ba2aac3d874c975d1
N'TCHADE ABED NEGO <abednegontchade9@gmail.com>
2026-04-10 10:25:16 +0100

    premier commit depuis la branch develop

--- notes.py
+++ notes.py
@@ -2,3 +2,12 @@ etudiants = []
 def ajouter_etudiants(nom, note):
 	etudiants.append({"nom": nom, "note": note})
 	print(f"Etudiant {nom} ajouté avec la note {note}.")
+
+def calcul_moyenne():
+	if not etuidants:
+	print ("Aucun étudiants enregistré.")
+	return 0
+     total= sul(e['note'] for e in entudiants)
+	moyenne= total/len(etudiants)
+	print(f"Moyenne de la classe :{moyenne:.2f}")
+	return moyenne
